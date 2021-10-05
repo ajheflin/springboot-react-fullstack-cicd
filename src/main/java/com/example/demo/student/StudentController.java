354c735838c7469a09c5f26df143f7e47c42aa5b
ajheflin <me@ajheflin.com>
2021-10-05 10:26:25 -0400

    Finished section 1

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.example.demo.student.Gender.FEMALE;
+import static com.example.demo.student.Gender.MALE;
+
+@RestController
+@RequestMapping(path = "/api/v1/students")
+public class StudentController {
+    @GetMapping("/")
+    public List<Student> getAllStudents() {
+        return Arrays.asList(
+                new Student(1L, "AJ Heflin", "me@ajheflin.com", MALE),
+                new Student(2L, "Rachel Hester", "racheyhester@gmail.com", FEMALE)
+        );
+    }
+}
